b63f1b195d3d548e2c8edd0d21db4399af16097a
Peakky <131934114+Peakky@users.noreply.github.com>
2023-06-13 20:20:25 +0700

    s

--- src/main/java/com/example/projectbd2/Supplier/SupplierController.java
+++ src/main/java/com/example/projectbd2/Supplier/SupplierController.java
@@ -1,4 +1,118 @@
 package com.example.projectbd2.Supplier;
 
-public class SupplierController {
+import com.example.projectbd2.HelloApplication;
+import com.example.projectbd2.Pagination;
+import com.example.projectbd2.Transaction.Transaction;
+import com.example.projectbd2.Transaction.Transaction_Property;
+import com.example.projectbd2.Transaction.Transaction_Repository;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class SupplierController implements Initializable {
+    @FXML
+    private Button ButtonNext, ButtonPrev;
+    @FXML
+    private TableColumn<SupplierProperty, String> IDSup, namaSup;
+    @FXML
+    private TableView<SupplierProperty> TableSupplier;
+
+    private int page = 1;
+
+    private int rowsPerPage = 12;
+
+    private ObservableList<SupplierProperty> supplier = FXCollections.observableArrayList();
+
+    private SupplierRepository supplierRepository = new SupplierRepository();
+
+    public SupplierController() throws SQLException {
+    }
+    @FXML
+    void onNextButtonClick(ActionEvent event) throws SQLException {
+        page++;
+        updateTable();
+    }
+
+    @FXML
+    void onPrevButtonClick(ActionEvent event) throws SQLException {
+        page--;
+        updateTable();
+    }
+
+    @FXML
+    void onBackButtonClick(){
+        HelloApplication app = HelloApplication.getApplicationInstance();
+        app.setPrimaryStage(app.getMenu());
+    }
+
+    @FXML
+    void onAddButtonClick(){
+        HelloApplication app = HelloApplication.getapplicationInstance();
+        app.setPrimaryStage(app.getTransactionInsert());
+    }
+
+    @FXML
+    void onEditButtonClick(){
+        HelloApplication app = HelloApplication.getApplicationInstance();
+        app.setPrimaryStage(app.getTransactionUpdate());
+    }
+
+    @FXML
+    void onDeleteButtonClick(){
+        HelloApplication app = HelloApplication.getApplicationInstance();
+        app.setPrimaryStage(app.getTransactionDelete());
+    }
+
+    private void updateButton() throws SQLException {
+        if(page <= 1){
+            ButtonPrev.setDisable(true);
+        } else {
+            ButtonPrev.setDisable(false);
+        }
+        if (page >= Math.ceil((supplierRepository.GetSupplierCount()  / Double.valueOf(rowsPerPage)))){
+            ButtonNext.setDisable(true);
+        } else {
+            ButtonNext.setDisable(false);
+        }
+    }
+
+    public void updateTable() throws SQLException {
+        supplier = FXCollections.observableArrayList();
+        ArrayList<Supplier> result;
+        try {
+            result = supplierRepository.GetSupplier(new Pagination(page, rowsPerPage));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        result.forEach((t) -> {
+            SupplierProperty tp = new SupplierProperty();
+            tp.setIdSup(Integer.toString(t.id));
+            tp.setNamaSup(t.namaSup);
+            supplier.add(tp);
+        });
+        TableSupplier.setItems(supplier);
+        updateButton();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        IDSup.setCellValueFactory(f -> f.getValue().idSupProperty());
+        namaSup.setCellValueFactory(f -> f.getValue().namaSupProperty());
+
+        try {
+            updateTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/com/example/projectbd2/Supplier/SupplierRepository.java
@@ -0,0 +1,85 @@
+package com.example.projectbd2.Supplier;
+
+import com.example.projectbd2.Pagination;
+import com.example.projectbd2.connection;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class SupplierRepository {
+    private Connection conn;
+
+    public SupplierRepository() throws SQLException {
+        conn = connection.GetConnection();
+    }
+
+    public int GetSupplierCount() throws SQLException {
+        Statement stmt = conn.createStatement();
+        String sql = "SELECT COUNT(id) FROM supplier";
+        System.out.println(sql);
+        ResultSet rs = stmt.executeQuery(sql);
+        if(rs.next()){
+            return rs.getInt(1);
+        } else{
+            return 0;
+        }
+    }
+
+    public ArrayList<Supplier> GetSupplier(Pagination pgn) throws SQLException {
+        ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
+        Statement stmt = conn.createStatement();
+        String sql = String.format("SELECT * FROM Supplier LIMIT %o OFFSET %o", pgn.limit,pgn.offset);
+        System.out.println(sql);
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()){
+            suppliers.add(
+                    new Supplier(
+                            rs.getInt("id"),
+                            rs.getString("Nama")
+                    )
+            );
+        }
+        return suppliers;
+    }
+
+    public void insertData(String Nama) throws SQLException {
+        String Query = "INSERT INTO Supplier (Nama) VALUES (?)";
+        PreparedStatement preparedStatement = conn.prepareStatement(Query);
+
+        preparedStatement.setString(1, Nama);
+
+        System.out.println(preparedStatement);
+        preparedStatement.execute();
+    }
+
+    public void updateData(int id,String Nama) throws SQLException {
+        String Query = "UPDATE Supplier SET ";
+        PreparedStatement preparedStatement = conn.prepareStatement(Query + "Nama = '" + Nama + "' where id = " + id);
+        System.out.println(preparedStatement);
+        preparedStatement.execute();
+
+    }
+    public void deleteData(int id) throws SQLException {
+        String Query = "DELETE FROM Supplier WHERE id = " + id;
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(Query);
+            System.out.println(preparedStatement);
+            preparedStatement.execute();
+        } catch (SQLException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    public boolean cekId(int id) throws SQLException {
+        String Query = "select * FROM Supplier WHERE id = " + id;
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(Query);
+            System.out.println(preparedStatement);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+        return true;
+    }
+}
